6ab9bf8e5d288ea02979cc4462fe2bbaa415977d
burato42 <tmahmutov@gmail.com>
2024-12-10 18:40:55 +0400

    Day10 formatting

--- day10/trails.go
+++ day10/trails.go
@@ -4,12 +4,11 @@ import (
 	"aoc2024/utils"
 )
 
-
 func Trail(matrix [][]int) int {
 	var dfs func([][]int, int, int)
 	res := [][2]int{}
-	
-	dfs = func (matrix [][]int, row int, col int) {
+
+	dfs = func(matrix [][]int, row int, col int) {
 
 		if matrix[row][col] == 9 {
 			res = append(res, [2]int{row, col})
@@ -17,13 +16,13 @@ func Trail(matrix [][]int) int {
 		}
 		dirs := [][]int{{1, 0}, {-1, 0}, {0, 1}, {0, -1}}
 		for _, d := range dirs {
-			if utils.IsInside(row + d[0], col + d[1], len(matrix), len(matrix[0])) && 
-			matrix[row + d[0]][col + d[1]] - matrix[row][col] == 1 {
-				dfs(matrix, row + d[0], col + d[1])
+			if utils.IsInside(row+d[0], col+d[1], len(matrix), len(matrix[0])) &&
+				matrix[row+d[0]][col+d[1]]-matrix[row][col] == 1 {
+				dfs(matrix, row+d[0], col+d[1])
 			}
 		}
 	}
-	
+
 	sum := 0
 	for row, line := range matrix {
 		for col, el := range line {
@@ -38,4 +37,3 @@ func Trail(matrix [][]int) int {
 
 	return sum
 }
-
